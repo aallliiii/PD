e6e909a8410dd035bfb9f44154a338e4d39acd6a
aallliiii <144407855+aallliiii@users.noreply.github.com>
2023-09-15 22:33:07 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+main()
+{
+system("color 09");
+cout<<"+--^----------,--------,-----,--------^-  "<<endl;
+cout<<" | |||||||||   '--------'     |         'O"<<endl;
+cout<<" '+---------------------------^----------|"<<endl;
+cout<<"   ',--------,-------------,-----------'  "<<endl;
+cout<<"     / XXXXXX /'|        /'               "<<endl;
+cout<<"    / XXXXXX /  |       /'                "<<endl;
+cout<<"   / XXXXXX /'---------'                  "<<endl;
+cout<<"  / XXXXXX /                              "<<endl;
+cout<<" / XXXXXX /                               "<<endl;
+cout<<"/ XXXXXX /                                "<<endl;
+cout<<"(--------(                                "<<endl;
+cout<<" '------'                                 "<<endl;
+
+}
